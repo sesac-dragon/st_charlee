94fc581c6c711f22e6a40dd73e87e2322479947c
nimowa03 <nimowa03@gmail.com>
2025-07-21 16:06:06 +0900

    커밋 수정

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st 
 
+import os 
+
 st.write('배포 테스트')
 st.write('배포 테스트용 앱입니다.')
+
+st.write('추가 커밋')
+
+api_key = os.environ.get("API_SECRET_KEY")
+
+st.write(f'api key = {api_key}')         
